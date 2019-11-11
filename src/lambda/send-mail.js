41394b58230c775acc5fd5c2c7361213c9589623
Chibuzor Obiora <nbyodai@gmail.com>
2019-11-11 12:28:15 +0100

    Fix forms

--- src/lambda/send-mail.js
+++ src/lambda/send-mail.js
@@ -2,25 +2,30 @@ const nodemailer = require("nodemailer");
 
 export function handler(event, context, callback) {
   const body = JSON.parse(event.body);
+  
+  const EMAIL_USERNAME = process.env.EMAIL_USERNAME
+  const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
+  const HELLO_EMAIL = process.env.HELLO_EMAIL
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    host: "smtp.gmail.com",
     auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+      user: EMAIL_USERNAME,
+      pass: EMAIL_PASSWORD,
     }
   });
 
   const mailOptions = {
     from: body.email,
-    to: process.env.HELLO_EMAIL,
+    to: HELLO_EMAIL,
     subject: `Hi, Aisha, ${body.name} would like to contact you`,
     html: `<p>${body.message}</p>`
   };
 
   transporter.sendMail(mailOptions, function(err, info) {
-    if (err) console.log(err);
-    else console.log(info);
+    if (err) console.error({ err });
+    else console.log({ info });
   });
 
   callback(null, {
