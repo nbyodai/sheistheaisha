179f84bb41c29795e2795ad240f17df590827703
Chibuzor Obiora <nbyodai@gmail.com>
2019-12-22 11:46:38 +0100

    Set up lambda and logic

--- src/lambda/send-mail.js
+++ src/lambda/send-mail.js
@@ -1,44 +1,35 @@
-const nodemailer = require("nodemailer");
+const mailgun = require("mailgun-js");
 
 export function handler(event, context, callback) {
   const body = JSON.parse(event.body);
-  
-  const EMAIL_USERNAME = process.env.EMAIL_USERNAME
-  const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
-  const HELLO_EMAIL = process.env.HELLO_EMAIL
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    auth: {
-      user: EMAIL_USERNAME,
-      pass: EMAIL_PASSWORD,
-    }
-  });
+  const HELLO_EMAIL = process.env.HELLO_EMAIL;
+  const API_KEY = process.env.MAILGUN_API_KEY;
+  const DOMAIN = process.env.MAILGUN_DOMAIN;
+  const URL = process.env.MAILGUN_URL;
 
-  const mailOptions = {
-    from: body.email,
+  const loaded = mailgun({ apiKey: API_KEY, domain: DOMAIN, url: URL });
+  const data = {
+    from: `Your Site Visitor <${body.email}>`,
     to: HELLO_EMAIL,
     subject: `Hi, Aisha, ${body.name} would like to contact you`,
+    text: `${body.message}`,
     html: `<p>${body.message}</p>`
   };
 
-  transporter.sendMail(mailOptions, function(err, info) {
-    if (err) {
-      console.error({ err });
+  loaded.messages().send(data, (error, body) => {
+    if (error) {
       callback(null, {
-        statusCode: 200,
+        statusCode: 400,
         body: JSON.stringify({
-          error: err
+          error: error
         })
       });
-    }
-    else {
-      console.log({ info })
+    } else {
       callback(null, {
         statusCode: 200,
         body: JSON.stringify({
-          info: 'Success'
+          info: body
         })
       });
     }
